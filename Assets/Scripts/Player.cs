c58125716f8731d97c7f938ab4a9092ecb842dab
Dylan Robles <77290019+dylanr150@users.noreply.github.com>
2025-03-02 21:37:07 -0800

    Got camera working with player movement/mouse determining direction of player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,14 @@
 using System;
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
+    [SerializeField] private CinemachineCamera thirdPersonCam;
+    float rotationSpeed = 10f;
     private Rigidbody rb;
 
     void Start()
@@ -19,9 +22,20 @@ public class Player : MonoBehaviour
 
     private void MovePlayer(Vector3 direction)
     {
-        Vector3 moveDirection = new(direction.x, 0f, direction.z);
-        rb.linearVelocity = new Vector3(moveDirection.x * speed, rb.linearVelocity.y, moveDirection.z * speed);
+        Vector3 camForward = thirdPersonCam.transform.forward;
+        camForward.y = 0f;
+        camForward.Normalize();
 
+        Vector3 camRight = thirdPersonCam.transform.right;
+        camRight.y = 0f;
+        camRight.Normalize();
+
+        Vector3 moveDirection = (camForward * direction.z + camRight * direction.x);
+
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+        rb.linearVelocity = new Vector3(moveDirection.x * speed, rb.linearVelocity.y, moveDirection.z * speed);
     }
     private void PlayerJump(Vector3 direction)
     {
@@ -29,7 +43,6 @@ public class Player : MonoBehaviour
         {
             Vector3 jumpDirection = direction;
             rb.AddForce(jumpHeight * jumpDirection);
-            Debug.Log("true");
         }
     }
 }
