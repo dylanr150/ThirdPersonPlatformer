85bee369b8075b6f07efdf02b987fad393404327
Dylan Robles <77290019+dylanr150@users.noreply.github.com>
2025-03-02 16:48:10 -0800

    Basic setup with player and floor along with basic movement.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    void Update()
+    {
+
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            input += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            input += Vector2.up;
+        }
+        OnMove?.Invoke(input);
+    }
+}
