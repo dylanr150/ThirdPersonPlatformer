798a51b0dc7d6da1a03909ea6302823064b2b005
Dylan Robles <77290019+dylanr150@users.noreply.github.com>
2025-03-02 19:58:04 -0800

    Adjusted movement script and added jumping
    Changed from addforce to use linear velocity for movement, this allows for more consistent movement when jumping with the player as well as more consisten movement in general apposed to only relying on physics material.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,30 +1,46 @@
 using UnityEngine;
 using System;
 using UnityEngine.Events;
+using Unity.VisualScripting;
 
 public class InputManager : MonoBehaviour
 {
-    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+    public UnityEvent<Vector3> OnJump = new UnityEvent<Vector3>();
+
     void Update()
     {
 
-        Vector2 input = Vector2.zero;
+        Vector3 input = Vector3.zero;
+        Vector3 jumpInput = Vector3.zero;
         if (Input.GetKey(KeyCode.A))
         {
-            input += Vector2.left;
+            input += Vector3.left;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            input += Vector2.right;
+            input += Vector3.right;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            input += Vector2.down;
+            input += Vector3.back;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            input += Vector2.up;
+            input += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jumpInput += Vector3.up;
         }
         OnMove?.Invoke(input);
+        OnJump?.Invoke(jumpInput);
+
+        
+    
+    }
+    void FixedUpdate()
+    {
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,35 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float speed;
-
+    [SerializeField] private float jumpHeight;
     private Rigidbody rb;
 
     void Start()
     {
         inputManager.OnMove.AddListener(MovePlayer); //Adding MovePlayer listener to the OnMove event.
+        inputManager.OnJump.AddListener(PlayerJump);
         rb = GetComponent<Rigidbody>();
 
 
     }
 
-    private void MovePlayer(Vector2 direction)
+    private void MovePlayer(Vector3 direction)
     {
-        Vector3 moveDirection = new(direction.x, 0f, direction.y);
-        rb.AddForce(speed * moveDirection);
+        Vector3 moveDirection = new(direction.x, 0f, direction.z);
+        rb.linearVelocity = new Vector3(moveDirection.x * speed, rb.linearVelocity.y, moveDirection.z * speed);
 
+    }
+    private void PlayerJump(Vector3 direction)
+    {
+        if (Physics.Raycast(transform.position, -Vector3.up, 1.1f))
+        {
+            Vector3 jumpDirection = direction;
+            rb.AddForce(jumpHeight * jumpDirection);
+            Debug.Log("true");
+        }
     }
 }
